1f5045318055e81dda278e9877007a7a23ca8807
COCONUT <64309783+rtq9731@users.noreply.github.com>
2022-07-02 11:10:50 +0900

    음양 더하기
    < https://programmers.co.kr/learn/courses/30/lessons/76501 >

---
+++ 04.PlusMinus/04.PlusMinus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _04.PlusMinus
+{
+    public class Solution
+    {
+        public int solution(int[] absolutes, bool[] signs)
+        {
+            int answer = 0;
+
+            for (int i = 0; i < absolutes.Length; i++)
+            {
+                answer += (signs[i] ? absolutes[i] : -absolutes[i]);
+            }
+
+            return answer;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+
+            sol.solution(new int[] { 4, 7, 12 }, new bool[] { true, false, true });
+        }
+    }
+}
