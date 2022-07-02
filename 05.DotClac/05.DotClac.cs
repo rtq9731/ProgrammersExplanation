4266ded98434b04d16e1a20d0750dc7e0154992d
COCONUT <64309783+rtq9731@users.noreply.github.com>
2022-07-02 11:11:06 +0900

    내적
    < https://programmers.co.kr/learn/courses/30/lessons/70128 >

---
+++ 05.DotClac/05.DotClac.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05.DotClac
+{
+    public class Solution
+    {
+        public int solution(int[] a, int[] b)
+        {
+            int answer = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                answer += a[i] * b[i];
+            }
+
+            return answer;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+
+            sol.solution(new int[] { 1, 2, 3, 4 }, new int[] { -3, -1, 0, 2 });
+        }
+    }
+}
